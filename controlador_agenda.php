9303159628d59265cf9bcdfb627e4a25d192d20d
biancamuniz <biancarolmuniz@gmail.com>
2017-07-06 13:28:11 -0300

    Update controlador_agenda.php

--- controlador_agenda.php
+++ controlador_agenda.php
@@ -1,35 +1,29 @@
 <?php
 
 	function abrirArquivo(){
-
 		$contatosAuxiliar = file_get_contents('contatos.json');
 		$contatosAuxiliar = json_decode($contatosAuxiliar, true);
-
 		return $contatosAuxiliar;
 	}
 
 	function salvarArquivo($contatosAuxiliar){
-
 		$contatosJson = json_encode($contatosAuxiliar, JSON_PRETTY_PRINT);
-
 		file_put_contents('contatos.json', $contatosJson);
 		
 		header("Location: index.phtml");
 	}
 
-	function cadastrar(){
+	function cadastrar($nome, $email, $telefone){
 			
 		$contatosAuxiliar = abrirArquivo();
-
 		$contato = [
-			'id'       => uniqid(),
-			'nome'     => $_POST['nome'],
-			'email'    => $_POST['email'],
-			'telefone' => $_POST['telefone']
-			];
+		 	'id'       => uniqid(),
+            'nome'     => $nome,
+            'email'    => $email,
+            'telefone' => $telefone
+        ];
 			
 		array_push($contatosAuxiliar, $contato);
-
 		salvarArquivo($contatosAuxiliar);
 	
 	}
@@ -37,16 +31,11 @@
 	function pegarContatos(){
 		
 		$contatosAuxiliar = abrirArquivo();
-		
 		return $contatosAuxiliar;
 		
 	}
-
 	function excluirContato($id){
-
 		$contatosAuxiliar = abrirArquivo();
-
-
 		print_r($contatosAuxiliar);
 		
 		foreach ($contatosAuxiliar as $posicao => $contato){ //iteração
@@ -59,41 +48,32 @@
 		salvarArquivo($contatosAuxiliar);
 	
 	}
-
 	function editarContato($id){
 		
 		$contatosAuxiliar = abrirArquivo();
-
 		foreach ($contatosAuxiliar as $contato){ //iteração
 			if ($contato['id'] == $id){
 				return $contato;
 			}
 		}
 	}
-
-	function SalvarContatoEditado($id){
-
+	function SalvarContatoEditado($id, $nome, $email, $telefone){
 		$contatosAuxiliar = abrirArquivo();
-
 		foreach ($contatosAuxiliar as $posicao => $contato){ //iteração
 			if ($contato['id'] == $id){
 				
-				$contatosAuxiliar[$posicao]['nome']     = $_POST['nome'];
-				$contatosAuxiliar[$posicao]['email']    = $_POST['email'];
-				$contatosAuxiliar[$posicao]['telefone'] = $_POST['telefone'];
-
+				$contatosAuxiliar[$posicao]['nome']     = $nome;
+             	$contatosAuxiliar[$posicao]['email']    = $email;
+             	$contatosAuxiliar[$posicao]['telefone'] = $telefone;
 				break;
 			}
 		}
-
 		salvarArquivo($contatosAuxiliar);
-
 	}
-
 	if ($_GET['acao'] == 'cadastrar') {
-		cadastrar();
+		cadastrar($_POST['nome'], $_POST['email'], $_POST['telefone']);
 	} elseif ($_GET['acao'] == 'excluir'){
 		excluirContato($_GET['id']);
 	} elseif ($_GET['acao'] == 'editar') {
-		SalvarContatoEditado($_POST['id']);
+		SalvarContatoEditado($_POST['id'], $_POST['nome'], $_POST['email'], $_POST['telefone']);
 	}
